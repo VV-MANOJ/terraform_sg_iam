3ca05355c0853ad58e257699c4d7ef7a7399a186
M@nojrpms <30459831+VV-MANOJ@users.noreply.github.com>
2023-06-23 06:35:08 -0700

    Update iam_role.tf

--- iam_role.tf
+++ iam_role.tf
@@ -20,3 +20,49 @@ resource "aws_iam_role_policy_attachment" "example_policy_attachment" {
   role       = aws_iam_role.example_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
 }
+
+resource "aws_iam_role" "example_role2" {
+  name               = "ExampleRole"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "example_policy_attachment2" {
+  role       = aws_iam_role.example_role2.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
+resource "aws_iam_role" "example_role3" {
+  name               = "ExampleRole"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "example_policy_attachment3" {
+  role       = aws_iam_role.example_role3.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
